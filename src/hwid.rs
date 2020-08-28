2a235c03bbc14e09c020b06a36dc410492a06480
mastercooker <c.o.okiedragon234@gmail.com>
2020-08-29 00:47:30 +0100

    HWID support

---
+++ src/hwid.rs
@@ -0,0 +1,60 @@
+
+#[cfg(target_os = "windows")]
+extern crate winreg;
+#[cfg(target_os = "windows")]
+use winreg::enums::HKEY_LOCAL_MACHINE;
+
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+use std::process::Command;
+use obfstr::{obfstr, obflocal};
+use std::borrow::Borrow;
+
+#[cfg(target_os = "windows")]
+pub fn get_id() -> std::string::String {
+	let hive = winreg::RegKey::predef(HKEY_LOCAL_MACHINE).open_subkey(obfstr!("\\\\SOFTWARE\\Microsoft\\Cryptography")).expect(obfstr!("Failed to open subkey"));
+	let id = hive.get_value(obfstr!("MachineGuid")).expect(obfstr!("Failed to get MachineGuid"));
+	return id
+}
+
+#[cfg(target_os = "macos")]
+pub fn get_id() -> std::string::String {
+	let output = Command::new(obfstr!("ioreg"))
+		.arg(obfstr!("-rd1")).arg(obfstr!("-c")).arg(obfstr!("IOExpertPlatformDevice"))
+		.output().expect(obfstr!("Failed to get HWID"));
+	return String::from_utf8(output.stdout).unwrap();
+}
+
+#[cfg(target_os = "linux")]
+pub fn get_id() -> std::string::String {
+	let mut id = String::new();
+	let mut path: String = obfstr!("/etc/machine-id").to_owned();
+	if Path::new(&path).exists() {
+		File::open(&path).unwrap().read_to_string(&mut id);
+	} else {
+		path = obfstr!("/etc/machine-id").to_owned();
+		if Path::new(&path).exists() {
+			File::open(&path).unwrap().read_to_string(&mut id);
+		}
+	}
+	
+	let output = Command::new(obfstr!("lsblk"))
+		.arg(obfstr!("-nro")).arg(obfstr!("SERIAL"))
+		.output().expect(obfstr!("Failed to get HWID"));
+	let diskSerial = String::from_utf8(output.stdout);
+	
+	if diskSerial.is_ok() {
+		id.push_str(diskSerial.unwrap().as_str());
+		return id;
+	}
+	return id;
+}
+
+#[cfg(target_os = "freebsd")]
+#[cfg(target_os = "dragonfly")]
+#[cfg(target_os = "openbsd")]
+#[cfg(target_os = "netbsd")]
+pub fn get_id() -> std::string::String {
+	unimplemented!("*BSD support is not implemented")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,48 @@
+mod hwid;
+mod utils;
 mod jvmti;
 
 extern crate jni;
 
+use hwid::get_id;
 use jni::JNIEnv;
-use jni::objects::{JClass, JString, JObject, GlobalRef};
-use jni::sys::{JavaVM, JNI_VERSION_1_8, JNI_OK, jint, jobject, jclass};
+use jni::objects::{JClass, JString, JObject, GlobalRef, ReleaseMode};
+use jni::sys::{JavaVM, JNI_VERSION_1_8, JNI_OK, jint, jobject, jclass, JNINativeInterface_};
 use std::ffi::CStr;
-use crate::jvmti::{jvmtiEnv, JVMTI_VERSION_1_2, jvmtiCapabilities, jvmtiError_JVMTI_ERROR_NONE, jvmtiEventCallbacks, jvmtiEventMode_JVMTI_ENABLE, jvmtiEvent_JVMTI_EVENT_CLASS_FILE_LOAD_HOOK};
-use std::ptr::null_mut;
+use crate::jvmti::{jvmtiEnv, JVMTI_VERSION_1_2, jvmtiCapabilities, jvmtiError_JVMTI_ERROR_NONE, jvmtiEventCallbacks, jvmtiEventMode_JVMTI_ENABLE, jvmtiEvent_JVMTI_EVENT_CLASS_FILE_LOAD_HOOK, jvmtiEventMode_JVMTI_DISABLE};
+use std::ptr::{null_mut};
 use std::os::raw::{c_void, c_int, c_uchar, c_char};
 use std::mem::{zeroed, size_of};
 use obfstr::{obfstr};
+use jni::strings::{JNIString, JNIStr};
 
-static mut G_BYTEMAP: Option<GlobalRef> = None;
+static mut G_BYTEMAP_REF: Option<GlobalRef> = None;
 
 
 #[no_mangle]
 pub unsafe extern "system" fn JNI_OnLoad(
 	mut _env: JavaVM, _reserved: &mut c_void) -> c_int {
 	JNI_VERSION_1_8
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_me_cookiedragon234_falcon_NativeAccessor_d<'a>(
+	env: JNIEnv<'a>, _class: JClass,
+	_1: JObject, _2: JObject, _3: JObject, _4: JObject) -> JString<'a> {
+	let hwid = get_id();
+	println!("Hwid: {}", hwid);
+	let str = env.new_string(JNIString::from(hwid));
+	return str.unwrap();
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_me_cookiedragon234_falcon_NativeAccessor_c(
+	env: JNIEnv, _class: JClass,
+	str: JObject) {
+	let jstring = JString::from(str);
+	let chars = env.get_string_utf_chars(jstring).unwrap();
+	println!("{} {}", obfstr!("[RAION]"), CStr::from_ptr(chars).to_str().unwrap());
+	env.release_string_utf_chars(jstring, chars).unwrap();
 }
 
 #[no_mangle]
@@ -29,7 +53,8 @@ pub unsafe extern "system" fn Java_me_cookiedragon234_falcon_NativeAccessor_b(
 	println!("Hello '{}'", CStr::from_ptr(chars).to_str().unwrap());
 	env.release_string_utf_chars(str, chars).unwrap();
 	
-	G_BYTEMAP = Some(env.new_global_ref(byte_map).unwrap());
+	let global_ref = env.new_global_ref(byte_map).unwrap();
+	G_BYTEMAP_REF = Some(global_ref);
 	
 	let mut void_ptr: *mut c_void = null_mut() as *mut c_void;
 	let penv_ptr: *mut *mut c_void = &mut void_ptr as *mut *mut c_void;
@@ -58,7 +83,7 @@ pub unsafe extern "system" fn Java_me_cookiedragon234_falcon_NativeAccessor_b(
 	
 	{
 		let mut callbacks: jvmtiEventCallbacks = zeroed();
-		callbacks.ClassFileLoadHook = Some(loadHookHandler);
+		callbacks.ClassFileLoadHook = Some(load_hook_handler);
 		
 		let result = (**jvmti).SetEventCallbacks.unwrap()(jvmti, &callbacks, size_of::<jvmtiEventCallbacks>() as i32);
 		if result != jvmtiError_JVMTI_ERROR_NONE {
@@ -76,19 +101,69 @@ pub unsafe extern "system" fn Java_me_cookiedragon234_falcon_NativeAccessor_b(
 	println!("{}", obfstr!("Raion native libary initialisation finished (4)"));
 }
 
-pub unsafe extern "C" fn loadHookHandler(
-	jvmti_env: *mut jvmtiEnv,
-	jni_env: *mut jni::sys::JNIEnv,
-	class_being_redefined: jclass,
-	loader: jobject,
+pub unsafe extern "C" fn load_hook_handler(
+	jvmti: *mut jvmtiEnv,
+	jni_env: *mut *const JNINativeInterface_,
+	_class_being_redefined: jclass,
+	_loader: jobject,
 	name: *const c_char,
-	protection_domain: jobject,
-	class_data_len: jint,
-	class_data: *const c_uchar,
+	_protection_domain: jobject,
+	_class_data_len: jint,
+	_class_data: *const c_uchar,
 	new_class_data_len: *mut jint,
 	new_class_data: *mut *mut c_uchar) {
 	if name.is_null() {
 		return;
 	}
-	println!("Class loaded {}", CStr::from_ptr(name).to_str().unwrap());
+	
+	let env: JNIEnv = JNIEnv::from_raw(jni_env).unwrap();
+	
+	let map_obj = G_BYTEMAP_REF.as_ref().unwrap().as_obj();
+	let map = env.get_map(map_obj).unwrap();
+	
+	let j_name = env.new_string(JNIStr::from_ptr(name).to_owned()).unwrap();
+	let bytes_option = map.remove(JObject::from(j_name)).unwrap();
+	if bytes_option.is_none() {
+		//println!("Bytes none {}", CStr::from_ptr(name).to_str().unwrap());
+		return;
+	}
+	let bytes = bytes_option.unwrap();
+	if bytes.is_null() {
+		//println!("Bytes null {}", CStr::from_ptr(name).to_str().unwrap());
+		return;
+	}
+	
+	let byte_num = env.get_array_length(bytes.into_inner()).unwrap();
+	let c_bytes = env.get_byte_array_elements(bytes.into_inner()).unwrap().0;
+	assert!(!c_bytes.is_null(), "Uh oh, bytes couldnt be fetched");
+	
+	{
+		let result = (**jvmti).Allocate.unwrap()(jvmti, byte_num as i64, new_class_data);
+		if result != jvmtiError_JVMTI_ERROR_NONE {
+			panic!("{} ({})", obfstr!("Couldn't allocate my stuffs"), result);
+		}
+	}
+	*new_class_data_len = byte_num;
+	
+	for i in 0..(byte_num as isize) {
+		let ptr = (*new_class_data).offset(i);
+		*ptr = *(c_bytes.offset(i)) as u8;
+		//set_u(*new_class_data, i, get_i(c_bytes, i) as u8);
+	}
+	
+	env.release_byte_array_elements(bytes.into_inner(), &mut *c_bytes, ReleaseMode::NoCopyBack).unwrap();
+	
+	//println!("Raion class loaded {}", CStr::from_ptr(name).to_str().unwrap());
+	
+	let map_size = env.call_method(map_obj, "size", "()I", &[]).unwrap().i().unwrap();
+	if map_size <= 0 {
+		// cleanup
+		let result = (**jvmti).SetEventNotificationMode.unwrap()(jvmti, jvmtiEventMode_JVMTI_DISABLE, jvmtiEvent_JVMTI_EVENT_CLASS_FILE_LOAD_HOOK, null_mut());
+		if result != jvmtiError_JVMTI_ERROR_NONE {
+			panic!("{} ({})", obfstr!("Couldn't disable jvmti callbacks"), result);
+		}
+		G_BYTEMAP_REF = None;
+		println!("{}", obfstr!("Raion has injected all components"));
+		return;
+	}
 }
